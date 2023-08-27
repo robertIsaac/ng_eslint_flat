ba915525150dcdbc0439b0a804cc035ea343a759
robert-isaac_emplc <robert.isaac@fastmarkets.com>
2023-08-27 21:38:50 +0300

    add the other libraries and rules
    - now we can safely delete the `.eslintrc.json` files from the project

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,17 @@ import ngParser from '@angular-eslint/template-parser';
 import js from '@eslint/js';
 import globals from 'globals';
 import ts from '@typescript-eslint/eslint-plugin';
+import ng from '@angular-eslint/eslint-plugin';
+import ngTeplate from '@angular-eslint/eslint-plugin-template';
+import esImport from 'eslint-plugin-import';
 
 export default [
   {
     files: ["**/*.ts"],
     plugins: {
       '@typescript-eslint': ts,
+      '@angular-eslint': ng,
+      'import': esImport,
     },
     languageOptions: {
       parser: tsParser,
@@ -26,12 +31,51 @@ export default [
       ...js.configs.recommended.rules,
       ...ts.configs['recommended-requiring-type-checking'].rules,
       ...ts.configs['stylistic-type-checked'].rules,
+      ...ng.configs.recommended.rules,
+      ...esImport.configs.errors.rules,
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          "type": "attribute",
+          "prefix": "app",
+          "style": "camelCase"
+        }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          "type": "element",
+          "prefix": "app",
+          "style": "kebab-case"
+        }
+      ],
+      "sort-imports": [
+        "error",
+        {
+          "ignoreDeclarationSort": true
+        }
+      ],
+      "import/no-unresolved": "off",
+      "import/newline-after-import": "error",
+      "import/order": [
+        "error",
+        {
+          "alphabetize": {"order": "asc"}
+        }
+      ]
     }
   },
   {
     files: ["**/*.html"],
+    plugins: {
+      '@angular-eslint/template': ngTeplate,
+    },
     languageOptions: {
       parser: ngParser,
+    },
+    rules: {
+      ...ngTeplate.configs.recommended.rules,
+      ...ngTeplate.configs.accessibility.rules,
     }
   },
   {
--- projects/ui/eslint.config.js
+++ projects/ui/eslint.config.js
@@ -12,5 +12,23 @@ export default [
         ]
       },
     },
+    "rules": {
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          "type": "attribute",
+          "prefix": "lib",
+          "style": "camelCase"
+        }
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          "type": "element",
+          "prefix": "lib",
+          "style": "kebab-case"
+        }
+      ]
+    }
   }
 ];
