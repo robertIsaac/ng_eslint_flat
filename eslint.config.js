994f86aba4cff1bf0e4471875ee3361ef56c48e3
robert-isaac_emplc <robert.isaac@fastmarkets.com>
2023-08-27 20:59:27 +0300

    add `eslint` recommended rules
    add globals to allow browser and jasmine global variables

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,33 @@
 import tsParser from '@typescript-eslint/parser';
 import ngParser from '@angular-eslint/template-parser';
+import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   {
     files: ["**/*.ts"],
     languageOptions: {
       parser: tsParser,
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
     }
   },
   {
     files: ["**/*.html"],
     languageOptions: {
       parser: ngParser,
+    }
+  },
+  {
+    files: ["**/*.spec.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.jasmine,
+      }
     }
   },
 ];
